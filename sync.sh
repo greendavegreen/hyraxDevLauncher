ad10f27312f37a95d92f5873ef864c18e909aae9
Dave Green <greendavegreen@gmail.com>
2017-03-11 11:20:36 -0500

    update with complete install of solr and removal of install tar

---
+++ sync.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+aws s3 sync . s3://hyrax-cf --profile dac --exclude ".git/*" --exclude "*.sh"
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 aws s3 sync . s3://hyrax-cf --profile dac \
                             --exclude ".git/*" --exclude "*.sh"
-aws cloudformation deploy --profile dac \
+aws cloudformation update-stack --profile dac \
                           --template-file stack.yaml \
                           --stack-name stack \
                           --capabilities CAPABILITY_IAM
